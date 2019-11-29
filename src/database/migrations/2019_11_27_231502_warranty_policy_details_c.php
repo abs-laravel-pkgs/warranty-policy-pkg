8d77ed724a6de8bc9c12ed310787c1e62b9da9da
Abdul <47030364+kaligan-uitoux@users.noreply.github.com>
2019-11-29 16:53:30 +0530

    Update 2019_11_27_231502_warranty_policy_details_c.php

--- src/database/migrations/2019_11_27_231502_warranty_policy_details_c.php
+++ src/database/migrations/2019_11_27_231502_warranty_policy_details_c.php
@@ -13,17 +13,17 @@ class WarrantyPolicyDetailsC extends Migration {
 	public function up() {
 		Schema::create('warranty_policy_details', function (Blueprint $table) {
 			$table->increments('id');
+			$table->unsignedInteger('warranty_policy_id');
 			$table->unsignedInteger('warranty_type_id');
 			$table->unsignedInteger('duration_type_id');
 			$table->unsignedDecimal('duration', 8, 2);
 			$table->string('more_info', 255)->nullable();
 			$table->unsignedTinyInteger('priority');
 
+			$table->foreign('warranty_policy_id')->references('id')->on('warranty_policies')->onDelete('CASCADE')->onUpdate('cascade');
 			$table->foreign('warranty_type_id')->references('id')->on('configs')->onDelete('CASCADE')->onUpdate('cascade');
 			$table->foreign('duration_type_id')->references('id')->on('configs')->onDelete('CASCADE')->onUpdate('cascade');
 
-			$table->dropForeign('warranty_policy_details_warranty_type_id_foreign');
-			$table->dropForeign('warranty_policy_details_duration_type_id_foreign');
 		});
 	}
 
