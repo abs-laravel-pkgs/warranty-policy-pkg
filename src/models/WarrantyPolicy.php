e431b302e475bafd9135add8e948e4785cf79a58
Ramakrishnan <45380286+ramuitoux@users.noreply.github.com>
2019-12-02 12:59:34 +0530

    Update WarrantyPolicy.php

--- src/models/WarrantyPolicy.php
+++ src/models/WarrantyPolicy.php
@@ -20,6 +20,69 @@ class WarrantyPolicy extends Model {
 		return $this->hasMany('Abs\WarrantyPolicyPkg\WarrantyPolicyDetail', 'warranty_policy_id')->orderBy('priority', 'asc');
 	}
 
+	public function getDaysByTwoDates($date1, $date2) {
+		$datetime1 = date_create($date1);
+		$datetime2 = date_create($date2);
+		$interval = date_diff($datetime1, $datetime2);
+		return $interval->days;
+	}
+
+	public function getWarrantyPolicyDetailStatus($battery_billed_date_format, $battery_billed_date, $battery_used_days) {
+		$response = array();
+		//GET WARRANTY POLICY DETAILS
+		if ($this->warrantyPolicyDetails) {
+			$total_battery_warranty_days = 0;
+			foreach ($this->warrantyPolicyDetails as $key => $warrantyPolicyDetail) {
+				//DURATION TYPE DAYS
+				if ($warrantyPolicyDetail->duration_type_id == 7260) {
+					$total_battery_warranty_days = intval($warrantyPolicyDetail->duration);
+				} elseif ($warrantyPolicyDetail->duration_type_id == 7261) {
+					//DURATION TYPE WEEKS
+					$total_battery_warranty_days = 7 * intval($warrantyPolicyDetail->duration);
+				} elseif ($warrantyPolicyDetail->duration_type_id == 7262) {
+					//DURATION TYPE MONTHS
+					$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " months", strtotime($battery_billed_date_format)));
+					$total_battery_warranty_days = $this->getDaysByTwoDates($battery_billed_date, $warranty_period_end_date);
+				} elseif ($warrantyPolicyDetail->duration_type_id == 7263) {
+					//DURATION TYPE YEARS
+					$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " year", strtotime($battery_billed_date_format)));
+					$total_battery_warranty_days = $this->getDaysByTwoDates($battery_billed_date, $warranty_period_end_date);
+				}
+
+				//IF BATTERY USED DAYS EXCEED WARRANRY DAYS
+				if ($battery_used_days > $total_battery_warranty_days) {
+					//IF NEXT WARRANTY POLICY DETAIL EXIST
+					$next_key = $key + 1;
+					if (isset($this->warrantyPolicyDetails[$next_key])) {
+						if ($battery_used_days > $total_battery_warranty_days) {
+							$response['battery_status'] = 'Warranty expired';
+						} else {
+							//FREE REPLACEMENT
+							if ($warrantyPolicyDetail->warranty_type_id == 7360) {
+								$response['battery_status'] = 'Free Replacement';
+							} else {
+								$response['battery_status'] = $warrantyPolicyDetail->more_info;
+							}
+						}
+					} else {
+						$response['battery_status'] = 'Warranty expired';
+					}
+				} else {
+					//FREE REPLACEMENT
+					if ($warrantyPolicyDetail->warranty_type_id == 7360) {
+						$response['battery_status'] = 'Free Replacement';
+					} else {
+						$response['battery_status'] = $warrantyPolicyDetail->more_info;
+					}
+					break;
+				}
+			}
+		} else {
+			$response['battery_status'] = 'No policy found';
+		}
+		return $response;
+	}
+
 	public static function createFromCollection($records, $company = null, $specific_company = null, $tc) {
 		foreach ($records as $key => $record_data) {
 			try {
