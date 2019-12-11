8d8329bd200290e89b3d255c8e94f84d3680df30
Ramakrishnan <45380286+ramuitoux@users.noreply.github.com>
2019-12-11 11:51:25 +0530

    Update WarrantyPolicy.php

--- src/models/WarrantyPolicy.php
+++ src/models/WarrantyPolicy.php
@@ -64,47 +64,39 @@ class WarrantyPolicy extends Model {
 		}
 		return $response;
 	}
+	public function getTotalBatteryWarrantyDays($battery_billed_date_format, $battery_billed_date, $warrantyPolicyDetail) {
+		$total_battery_warranty_days = 0;
+		//DURATION TYPE DAYS
+		if ($warrantyPolicyDetail->duration_type_id == 7260) {
+			$total_battery_warranty_days = intval($warrantyPolicyDetail->duration);
+		} elseif ($warrantyPolicyDetail->duration_type_id == 7261) {
+			//DURATION TYPE WEEKS
+			$total_battery_warranty_days = 7 * intval($warrantyPolicyDetail->duration);
+		} elseif ($warrantyPolicyDetail->duration_type_id == 7262) {
+			//DURATION TYPE MONTHS
+			$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " months", strtotime($battery_billed_date_format)));
+			$total_battery_warranty_days = $this->getDaysBetweenTwoDates($battery_billed_date, $warranty_period_end_date);
+		} elseif ($warrantyPolicyDetail->duration_type_id == 7263) {
+			//DURATION TYPE YEARS
+			$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " year", strtotime($battery_billed_date_format)));
+			$total_battery_warranty_days = $this->getDaysBetweenTwoDates($battery_billed_date, $warranty_period_end_date);
+		}
+		return $total_battery_warranty_days;
 
+	}
 	public function getWarrantyPolicyDetailStatus($battery_billed_date_format, $battery_billed_date, $battery_used_days) {
 		$response = array();
 		//GET WARRANTY POLICY DETAILS
 		if ($this->warrantyPolicyDetails) {
-			$total_battery_warranty_days = 0;
 			foreach ($this->warrantyPolicyDetails as $key => $warrantyPolicyDetail) {
-				//DURATION TYPE DAYS
-				if ($warrantyPolicyDetail->duration_type_id == 7260) {
-					$total_battery_warranty_days = intval($warrantyPolicyDetail->duration);
-				} elseif ($warrantyPolicyDetail->duration_type_id == 7261) {
-					//DURATION TYPE WEEKS
-					$total_battery_warranty_days = 7 * intval($warrantyPolicyDetail->duration);
-				} elseif ($warrantyPolicyDetail->duration_type_id == 7262) {
-					//DURATION TYPE MONTHS
-					$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " months", strtotime($battery_billed_date_format)));
-					$total_battery_warranty_days = $this->getDaysBetweenTwoDates($battery_billed_date, $warranty_period_end_date);
-				} elseif ($warrantyPolicyDetail->duration_type_id == 7263) {
-					//DURATION TYPE YEARS
-					$warranty_period_end_date = date('d-m-Y', strtotime("+" . intval($warrantyPolicyDetail->duration) . " year", strtotime($battery_billed_date_format)));
-					$total_battery_warranty_days = $this->getDaysBetweenTwoDates($battery_billed_date, $warranty_period_end_date);
-				}
-
+				//GET TOTAL BATTERY WARRANTY DAYS
+				$total_battery_warranty_days = $this->getTotalBatteryWarrantyDays($battery_billed_date_format, $battery_billed_date, $warrantyPolicyDetail);
 				//IF BATTERY USED DAYS EXCEED WARRANRY DAYS
 				if ($battery_used_days > $total_battery_warranty_days) {
 					//IF NEXT WARRANTY POLICY DETAIL EXIST
 					$next_key = $key + 1;
 					if (isset($this->warrantyPolicyDetails[$next_key])) {
-						if ($battery_used_days > $total_battery_warranty_days) {
-							$response['battery_status'] = 'Warranty expired';
-							$response['warranty_policy_detail_id'] = $warrantyPolicyDetail->id;
-						} else {
-							//FREE REPLACEMENT
-							if ($warrantyPolicyDetail->warranty_type_id == 7360) {
-								$response['battery_status'] = 'Free Replacement';
-								$response['warranty_policy_detail_id'] = $warrantyPolicyDetail->id;
-							} else {
-								$response['battery_status'] = $warrantyPolicyDetail->more_info;
-								$response['warranty_policy_detail_id'] = $warrantyPolicyDetail->id;
-							}
-						}
+						continue;
 					} else {
 						$response['battery_status'] = 'Warranty expired';
 						$response['warranty_policy_detail_id'] = $warrantyPolicyDetail->id;
